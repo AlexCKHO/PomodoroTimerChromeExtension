4dd0618825c63837528bd1111652ef2775294ec5
Alex Ho <cho@spartaglobal.com>
2025-02-12 20:20:54 +0000

    Persist and retrieve previous state!

--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,29 @@
-const tasks = [];
+let tasks = [];
 
 const addTaskBtn = document.getElementById("add-task-btn");
 addTaskBtn.addEventListener("click", () => addTask());
 
-function addTask() {
-  const taskNum = tasks.length;
-  tasks.push("");
+chrome.storeage.sync.get(["tasks"], (res) => {
+  tasks = res.tasks ? res.tasks : [];
+  renderTasks();
+});
 
+function saveTasks() {
+  chrome.storage.sync.set({
+    task,
+  });
+}
+
+function renderTask(taskNum) {
   const taskRow = document.createElement("div");
-
   const text = document.createElement("input");
   text.type = "text";
   text.placeholder = "Enter a task...";
-
+  text.value = tasks[taskNum];
+  // When user add value in a input field it will update the tasks[]
   text.addEventListener("change", () => {
     tasks[taskNum] = text.value;
-    console.log(tasks);
+    saveTasks();
   });
 
   const deleteBtn = document.createElement("input");
@@ -23,12 +31,36 @@ function addTask() {
   deleteBtn.value = "X";
 
   deleteBtn.addEventListener("click", () => {
-    tasks.splice(taskNum, 1);
+    deleteTask(taskNum);
   });
 
   taskRow.appendChild(text);
   taskRow.appendChild(deleteBtn);
 
+  //Adding the newly created taskRow into the document
   const taskContainer = document.getElementById("task-container");
   taskContainer.appendChild(taskRow);
+}
+
+function addTask() {
+  //Update the local save
+
+  const taskNum = tasks.length;
+  tasks.push("");
+  renderTask(taskNum);
+  saveTasks();
+}
+
+function deleteTask(taskNum) {
+  tasks.splice(taskNum, 1);
+  renderTasks(taskNum);
+  saveTasks();
+}
+
+function renderTasks() {
+  const taskContainer = document.getElementById("task-container");
+  taskContainer.textContent = "";
+  tasks.forEach((taskText, taskNum) => {
+    renderTask(taskNum);
+  });
 }
